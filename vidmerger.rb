9fd1d89fc3617aa827dbb80e1040361d5f58254d
Thomas Gotwig <tgotwig@gmail.com>
2021-12-22 10:06:52 +0100

    Add depends_on to vidmerger.rb

--- vidmerger.rb
+++ vidmerger.rb
@@ -3,6 +3,8 @@ class Vidmerger < Formula
   homepage "https://github.com/TGotwig/vidmerger"
   url "https://github.com/TGotwig/vidmerger/releases/download/0.1.6/vidmerger-mac.tar.gz"
   sha256 "3b7cd38c5267f6c5202f36110f30a8fa880910ca913a10ce6c2363a4919c0951"
+  
+  depends_on "ffmpeg"
 
   def install
     bin.install "vidmerger"
