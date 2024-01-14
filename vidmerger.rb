a5adb33d889f438b0167d9bf16a850cebc4a5c30
♠️ Thomas Gotwig ♣️ <ramble.clef0y@icloud.com>
2024-01-14 14:14:35 +0100

    Fix sha256

--- vidmerger.rb
+++ vidmerger.rb
@@ -3,7 +3,7 @@ class Vidmerger < Formula
   homepage "https://github.com/TGotwig/vidmerger"
   url "https://github.com/tgotwig/vidmerger/releases/download/0.3.2/vidmerger-mac.tar.gz"
   version "0.3.2"
-  sha256 "1bd6dbf88430776d9cd7475a992f4c133c9e93662973d8d2c9b420f773177f9c"
+  sha256 "be9702bb675a7b88a652f9af6f01dc8377aaf107bf897abb13678fe2cd3ce52d"
 
   depends_on "ffmpeg"
 
